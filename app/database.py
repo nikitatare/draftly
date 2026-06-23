051b9211b5ecf07421847ba61099f301bb1e9ab6
Nikita Tare <nikitatare319@gmail.com>
2026-06-23 22:27:17 +0530

    updated sqlite

--- app/database.py
+++ app/database.py
@@ -6,7 +6,7 @@ import os
 load_dotenv(override=True)
 
 DATABASE_URL = os.getenv("DATABASE_URL")
-
+print("DATABASE_URL =", DATABASE_URL)
 engine = create_engine(DATABASE_URL,echo=True)
 
 SessionLocal = sessionmaker(
